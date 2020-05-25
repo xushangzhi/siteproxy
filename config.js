0975c4a6819f4066c1a94c3cda08c68ee7dd9321
kju157 <65396700+kju157@users.noreply.github.com>
2020-05-25 14:48:40 +0800

    Update config.js
    添加servername

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'proxy.netptop.com',
+    serverName: 'urunproxy-a9v6sqv73.now.sh',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
